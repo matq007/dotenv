220426a3514c1da669dbe8193cb18672e8361a09
Martin Proks <mproksik@gmail.com>
2020-10-01 23:09:39 +0200

    fix: required paths for packages

--- .config/conf.d/variables.fish
+++ .config/conf.d/variables.fish
@@ -1,14 +1,10 @@
 # Variables
-set -g -x EDITOR "vim"
-set -g -x PYTHONIOENCODING "UTF-8"
-set -g -x LANG "en_US.UTF-8"
-set -g -x LC_ALL "en_US.UTF-8"
+set -gx EDITOR "vim"
+set -gx PYTHONIOENCODING "UTF-8"
+set -gx LANG "en_US.UTF-8"
+set -gx LC_ALL "en_US.UTF-8"
 
-# Paths
-# set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-# set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
-# set -g fish_user_paths "/usr/local/opt/libxml2/bin" $fish_user_paths
-# set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
-set -g fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
